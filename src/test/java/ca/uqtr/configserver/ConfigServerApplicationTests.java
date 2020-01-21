7dacd4b9b05ef9ffc91cb5f0e4216f0f366bd909
zinnour <zinnour@uqtr.ca>
2020-01-21 16:18:29 -0500

    first commit

---
+++ src/test/java/ca/uqtr/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.uqtr.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
