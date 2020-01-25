db80e4ba5f961e89becdf570140c0794d675fc57
zinnour <46807690+zinnourlahcene@users.noreply.github.com>
2020-01-25 20:16:27 +0000

    heroku 2

--- src/main/java/ca/uqtr/configserver/ConfigServerApplication.java
+++ src/main/java/ca/uqtr/configserver/ConfigServerApplication.java
@@ -11,11 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableConfigServer
 @EnableEurekaClient
 @SpringBootApplication
-@Controller
+@RestController
 @ComponentScan(basePackages={"ca.uqtr"})
 public class ConfigServerApplication {
 
